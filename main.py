c46905ffa6e8a94b07a2109c3b1c896b9c1d35d4
Ajay <ajay.codes.dev@gmail.com>
2026-08-04 14:38:22 +0530

    Refactor main.py to use ArthurAssistant class

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from tools.tool_control import TimeStamp
 from agents.arthur import LocalArthur
 from agents.arthur import Arthur
 
-arthur = Arthur()
-local_arthur = LocalArthur()
+class ArthurAssistant:
+    def __init__(self):
+        self.arthur = Arthur()
+        self.local_arthur = LocalArthur()
 
-while True:
-    user = input("you: ")
-    print(arthur.model(user))
+    def main(self):
+        print("Arthur is running...")
+        while True:
+            user = input("\n You: ").lower()
+            if user in ["exit", "quit"]:
+                print("GoodBy")
+                break
+            if not user:
+                continue
+
+            response = self.arthur.model(user)
+            print(response)
+            if "rate limit" in response:
+                print("Cloud limit reached. Switching to local model...")
+                print("Type (cloud) to switch model")
+                while True:
+                    user = input("\n You: ").lower()
+                    if user in ["exit", "quit"]:
+                        print("GoodBy")
+                        break
+                    if user in ["cloud"]:
+                        break
+                    if not user:
+                        continue
+
+                    response = self.local_arthur.model(user)
+                    print(response)
+
+
+
+assisten = ArthurAssistant()
+assisten.main()
